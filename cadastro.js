1eeca5048c1fbf333e3f6cb0a9fcde50d5b0b966
milyzinhaah <Amorim.hemily@escola.pr.gov.br>
2025-11-06 21:11:27 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+const primeiroNome = "José Luiz"
+const sobrenome = "Ferreira"
+let idade = 18
+let nomeCompleto=`${primeiroNome} ${sobrenome}`;
+console.log(`nomeCompleto : ${nomeCompleto}`);
+console.log(`idade do usuário :${idade}`);
+idade ++
+console.log(`Aniversário do usuário: ${idade}`)
+nomeCompleto = "Maria Laura"
+console.log(nomeCompleto)
